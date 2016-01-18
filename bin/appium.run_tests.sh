288951f1430333effdd688b64ad8b570edd4169b
Vladimir Bilyov <vladimir.bilyov@gmail.com>
2016-01-18 09:45:15 +0300

    updated

--- bin/appium.run_tests.sh
+++ bin/appium.run_tests.sh
@@ -8,71 +8,88 @@ fi
 iOSWorkspaceDir=$2
 scheme=$1
 
-#testsGit="https://github.com/RedPlanetTesting/RedPlanetApp.git"
-testsGit="rphlTests_bare"
-
-
-#---------
-rphlTestsDir="rphlTests"
-
+rphlTestsDir="AppiumTests"
 xcBuildEnvFile="$iOSWorkspaceDir/bin/env.$scheme.sh"
+
+#Load script env settings
+envFile=`dirname $0`/env.sh
+
+if [ -f $envFile ]; then
+    . $envFile
+else
+    echo "$envFile doesn't exist. See `dirname $0`/env.sample.sh as an example"
+    exit
+fi
 
 function appium_start {
 
-/Applications/Appium.app/Contents/Resources/node/bin/node \
-	/Applications/Appium.app/Contents/Resources/node_modules/appium/bin/appium.js \
+    $BIN_NODE \
+	$APPIUM_JS \
 	--native-instruments-lib \
 	--address 127.0.0.1 \
-	--port 4723 \
+	--port $appiumPort \
+	--bootstrap-port $appiumBootstrapPort \
+	--tmp $appiumTmpDir \
 	--log-level info:info > /tmp/appium.log 2>&1 &
 	
 #sleep 30
 }
 
 function appium_stop {
-    killall node
+    #kill by port
+    pid=$(lsof -i tcp:$appiumPort -t);
+    kill -TERM $pid || kill -KILL $pid
 }
 
 function build_simulator {
-xcBuildEnvFile=$1
-iOSWorkspaceDir=$2
+    xcBuildEnvFile="$1"
+    iOSWorkspaceDir="$2"
 
-pushd  "$iOSWorkspaceDir"
-`dirname $0`/xc.build_simulator.sh "$xcBuildEnvFile" .
-result=$?
-if [ $result != 0 ]; then
-    "echo failed to build ios app"
-    exit 1
-fi
-popd
-
+    pushd  "$iOSWorkspaceDir"
+    `dirname $0`/xc.build_simulator.sh "$xcBuildEnvFile" .
+    result=$?
+    if [ $result != 0 ]; then
+        "echo failed to build ios app"
+        exit 1
+    fi
+    popd
 }
 
 function run_tests {
-rphlTestsDir=$1
+    rphlTestsDir=$1
 
-pushd "$rphlTestsDir"
-rm -rf "RP Staging.app"
-cp -r "/tmp/1/Build/Products/Debug-iphonesimulator/RP Staging.app" .
-popd
+    pushd "$rphlTestsDir"
+    rm -rf "RP Staging.app"
+#    cp -r "/tmp/1/Build/Products/Debug-iphonesimulator/RP Staging.app" .
+    ln -s "/tmp/1/Build/Products/Debug-iphonesimulator/RP Staging.app" "RP Staging.app"
+#    popd
 
-pushd "$rphlTestsDir/RedPlanetTest"
-ant Mobile
-#./run.sh
+#    pushd "$rphlTestsDir/RedPlanetTest"
+    ant Mobile
+    #./run.sh
 
-popd
+    popd
+}
 
+function cloneTests {
+    if [ -d "$rphlTestsDir" ]; then
+        pushd $rphlTestsDir
+        git pull
+        popd
+    else
+        rm -rf "$rphlTestsDir"
+        git clone "$testsGit" "$rphlTestsDir"
+    fi
 }
 
 appium_stop
 appium_start
 
-#build_simulator $xcBuildEnvFile $iOSWorkspaceDir
+#exit
+
+build_simulator "$xcBuildEnvFile" "$iOSWorkspaceDir"
     
-#clone tests
-rm -rf "$rphlTestsDir"
-#git clone "$testsGit" "$rphlTestsDir"
-cp -r 
+#cloneTests
 
 run_tests $rphlTestsDir
 
---
+++ bin/env.sample.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+export testsGit="https://github.com/v0l0d/RedPlanetApp.git"
+export appiumPort=4725
+export appiumBootstrapPort=4727
+export appiumTmpDir="/tmp/appium-ios-tmp"
+
+export BIN_NODE=/Applications/Appium.app/Contents/Resources/node/bin/node
+export APPIUM_JS=/Applications/Appium.app/Contents/Resources/node_modules/appium/bin/appium.js
