45f0c3e3d719facaf95dabe08529076771be7faa
Vladimir Bilyov <vladimir.bilyov@gmail.com>
2018-03-09 12:12:56 +0200

    changed verisioning

--- bin/git.build_number.sh
+++ bin/git.build_number.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-git rev-list HEAD --count
+git rev-list --count HEAD
 
+#https://stackoverflow.com/questions/4120001/what-is-the-git-equivalent-for-revision-number
+# this one didn't work well with build that has been merged after other commits, thou it shows previos version
+#git rev-list --count --first-parent HEAD
